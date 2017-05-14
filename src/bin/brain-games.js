9ed2d084a0ef231cec0f6a073e0fe2a707a8546d
Pavel Perminov <pavel@perminov.im>
2017-05-14 16:34:27 +0500

    stage 2

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,8 +1,2 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-
-const out = (line) => console.log(line);
-
-out('Welcome to the Brain Games!')
-const commoname = readlineSync.question('May I have your name, please? ');
-out('Welcome sir '+commoname+'!!!!!!1')
+import stealth from '..';
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+const out = (line) => console.log(line);
+
+out('Welcome to the Brain Games!')
+const commoname = readlineSync.question('May I have your name, please? ');
+out('Welcome sir '+commoname+'!!!!!!1')
