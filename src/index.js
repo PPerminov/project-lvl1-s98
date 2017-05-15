5e1a1aa01e770250a36ccf02f7b0747defb7f434
Pavel Perminov <pavel@perminov.im>
2017-05-15 13:43:39 +0500

    Added: 1. brain-even game

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import readlineSync from 'readline-sync';
 
-const out = (line) => { console.log(line); };
-
-export default () => {
-  out('Welcome to the Brain Games!\n');
-  const commoname = readlineSync.question('May I have your name? ');
-  out(`Hello, ${commoname}!`);
+const out = (line) => {
+  console.log(line);
 };
 
+export const greetings = (type = 'welcome') => {
+  out('Welcome to the Brain Games!\n');
+  if (type === 'greetings') {
+    const commoname = readlineSync.question('May I have your name? ');
+    out(`Hello, ${commoname}!`);
+  }
+};
+
+export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - (min + 1))) + min;
+
+
+export const gameEven = (cycles) => {
+
+};
 // export default greetings
