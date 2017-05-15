2a5c9b0a41cbf37236ff659ff6a7ccf104ee077a
Pavel Perminov <pavel@perminov.im>
2017-05-15 20:51:55 +0500

    Added: 1. greeting function

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
 import stealth from '..';
+
+stealth.greeting();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const out = (line) => console.log(line);
+// const out = (line) => console.log(line);
 
-out('Welcome to the Brain Games!')
-const commoname = readlineSync.question('May I have your name, please? ');
-out('Welcome sir '+commoname+'!!!!!!1')
+const greeting = () => {
+  console.log('Welcome to the Brain Games!')
+  const commoname = readlineSync.question('May I have your name, please? ');
+  console.log('Welcome sir !!!!!!1')
+}
